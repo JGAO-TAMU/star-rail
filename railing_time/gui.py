6e532f07ee402226564f08573150c11b84b30d62
JGAO-TAMU <156945517+JGAO-TAMU@users.noreply.github.com>
2024-08-25 22:18:17 -0500

    all my stuff so far

---
+++ railing_time/gui.py
@@ -0,0 +1,85 @@
+import tkinter as tk
+from tkinter import ttk
+import hsr as hsr
+
+def run_hsr_script(path, mode, material):
+    hsr.star_rail_dailies(path, mode, material)
+
+def update_paths(): 
+    global update_message_var
+    hsr_path = hsr_entry.get()
+    update_message_var.set(f"path updated to {hsr_path}") 
+
+def change_mode(event=None):
+    global modes, mode, combo, materials
+    mode = combo.get()
+    if(mode == 'crimson'):
+        new_materials = ['arrow_of_the_starchaser', 'countertemporal_shot', 'divine_amber', 'exquisite_colored_draft', 'flower_of_eternity', 'heaven_incinerator', 'key_of_wisdom', 'moon_rage_fang', 'obisidian_of_obsession', 'safeguard_of_amber', 'myriad_fruit', 'worldbreaker_blade']
+    elif(mode == 'shadow'):
+        new_materials = ['dream_flamer', 'raging_heart', 'IPC_work_permit']
+    elif(mode == 'golden'):
+        new_materials = ['none']
+    elif(mode == 'corrosion'):
+        new_materials=['hackerspace']    
+    combo2['values'] = new_materials
+
+def change_material(event=None):
+    global mode, materials, material, combo2
+    if(mode == 'crimson'):
+        material = 'crimson\\' + combo2.get()
+    #material = combo2.get()
+
+def main():
+
+    global hsr_path, hsr_entry, root, message_exists, update_message_var, modes, materials, mode, material, combo, combo2
+
+    modes = ['crimson', 'shadow', 'golden', 'corrosion']
+    materials = ['select a mode first']
+    mode = 'crimson' #default selections
+    material = 'dream_flamer'
+
+    message_exists = False
+    hsr_path = "D:\HSR\Star Rail\Games\StarRail.exe"
+    root=tk.Tk()
+    root.title("my scripts")
+    root.geometry("800x600")
+
+    canvas = tk.Canvas(root, width=100, height=100)
+    canvas.pack()
+
+    #smiley
+    canvas.create_oval(25,25,75,75)
+    canvas.create_line(40,40,40,45)
+    canvas.create_line(60,40,60,45)
+    canvas.create_line(30,60,70,60)
+    canvas.create_line(30,60,30,55)
+    canvas.create_line(70,60,70,55)
+
+    tk.Label(root, text="star rail path:").pack()
+    hsr_entry = tk.Entry(root)
+    hsr_entry.insert(0, hsr_path)
+    hsr_entry.pack()
+    btn_update = tk.Button(root, text="update path variable", command=update_paths).pack()
+    update_message_var = tk.StringVar()
+    update_message = tk.Message(root, textvariable=update_message_var).pack()
+
+    btn1 = tk.Button(root, text="play star rail", command=lambda: run_hsr_script(hsr_path, mode, material)).pack()
+
+    btn2 = tk.Button(root, text="play star rail", command=lambda: run_hsr_script(hsr_path, mode, material)).pack()
+
+    btn3 = tk.Button(root, text="play star rail", command=lambda: run_hsr_script(hsr_path, mode, material)).pack()
+    
+    combo= ttk.Combobox(root, values=modes)
+    combo.pack()
+    combo.bind("<<ComboboxSelected>>", change_mode)
+    
+    combo2= ttk.Combobox(root, values=materials)
+    combo2.pack()
+    combo2.bind("<<ComboboxSelected>>", change_material)
+
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
---
+++ railing_time/hsr.py
@@ -0,0 +1,196 @@
+import pyautogui
+import pydirectinput
+import time
+from PIL import Image
+from PIL import ImageChops
+from pywinauto import application
+from pywinauto import timings
+
+def click(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+def click_image(image_path, confidence=0.8, timeout=30, xoff=0, yoff=0, find_domain=False, searchtime=0.2, grayscale=False, movecamera=False, in_overworld=False):
+    print(f"")
+    print(f"click image called: image_path={image_path}, confidence={confidence}, timeout={timeout}, xoff={xoff}, yoff={yoff}, find_domain={find_domain}, searchtime={searchtime}, grayscale={grayscale}, movecamera={movecamera}")
+
+    start_time = time.time()
+    while True:
+
+        try:
+            location = pyautogui.locateCenterOnScreen(image_path, minSearchTime=searchtime, confidence=confidence, grayscale=grayscale)
+            if location:
+                #print("location")
+                x, y = location
+                #found_confidence = location.confidence   -----------   *problem line*
+                x += xoff
+                y += yoff
+                print(f"found image={image_path} and clicking offset x={xoff} y={yoff}")
+                if(in_overworld):
+                    pyautogui.keyDown('altleft')
+                    time.sleep(0.5)
+                pyautogui.click(x, y)
+                if(in_overworld):
+                    pyautogui.keyUp('altleft')
+                return True
+            
+        except Exception as e:
+            #print(f"Exception occurred")
+            
+            if(find_domain):
+                
+                try:
+                    temp = pyautogui.locateCenterOnScreen('railing_time\\railing_images\\enter.png', grayscale=True, confidence=0.8)
+                
+                except Exception as e:
+                    print("enter not found")
+                    try:
+                        temp = pyautogui.locateCenterOnScreen('railing_time\\railing_images\\teleport.png', grayscale=True, confidence=0.8)
+                    except Exception as e:
+                        temp=None
+                        print("teleport not found")    
+
+                if temp:
+                    pyautogui.moveTo(temp)
+                    pyautogui.scroll(-1)
+                    pyautogui.scroll(-1)
+                    pyautogui.scroll(-1)
+                    print("scrolled down")
+                
+                
+            if(movecamera):
+                pyautogui.FAILSAFE = False #Danger
+                pyautogui.moveRel(yOffset=100, duration=0.5)
+                pyautogui.FAILSAFE = True
+                print(f"moved camera y={100}") #change with yOffset
+
+            continue        
+
+        if time.time() - start_time > timeout:
+            print("click image timeout")
+            return False
+        
+        time.sleep(0.5)  
+
+def wait_for_screen(image_path, timeout=240, confidence=0.8, period=5):
+    start_time = time.time()
+    while True:
+        try:
+            print(f"waiting for image {image_path}...")
+            if pyautogui.locateCenterOnScreen(image_path, confidence=confidence):
+                return True
+        except Exception as e:
+            #print(f"Exception occurred: {e}")
+            elapsed_time = time.time() - start_time
+            print(f"Elapsed time: {elapsed_time}")
+        if elapsed_time > timeout:
+            print("screen timeout")
+            return False
+        time.sleep(period)
+        continue
+        
+        
+        
+
+def restart(): # call when the restart button appears
+    
+    print("Restarting...")
+    click_image('railing_time\\railing_images\one_more_time.png')
+    time.sleep(1)   # Wait for restart to complete
+
+
+app_path = "D:\HSR\Star Rail\Games\StarRail.exe" #path for my installation. users need to update this thru gui. gui does not remember changes for now. 
+
+def star_rail_dailies(path, mode, material):
+    print(path, mode, material)
+    
+    ############ start game ############
+    application.Application().start(path)
+    #main_window = app.window(title=window_title)
+    count = 1
+
+    click_image('railing_time\\railing_images\start_game_small.png',confidence=.9)
+    click_image('railing_time\\railing_images\\click_to_start.png',confidence=.9)
+
+    ############ overworld #############
+    #movecamera will happen while game is loading
+    click_image('railing_time\\railing_images\\daily_icon_unclaimed.png',confidence=.8, grayscale=True, movecamera=True, in_overworld=True)
+
+    time.sleep(2)
+    click_image('railing_time\\railing_images\\arrow_icon.png',confidence=.8)
+    print("arrow icon clicked")
+
+    ############# domain select ############
+    click_image(f'railing_time\\railing_images\\domain_select\\{mode}.png',confidence=.7)
+    click_image(f'railing_time\\railing_images\\domain_select\\{material}.png',confidence=.7, xoff=450, find_domain=True)
+
+    if(mode == 'crimson'):
+        click_image('railing_time\\railing_images\\add_runs.png',confidence=.97)
+        pyautogui.click()
+        pyautogui.click()
+        pyautogui.click()
+        pyautogui.click()
+
+    click_image('railing_time\\railing_images\\challenge.png',confidence=.9, searchtime=10)
+
+    ################ team select ##############
+    ### skip for now ###
+    """    
+    click_image('railing_time\\railing_images\\support_button.png',confidence=.9)
+    click_image('railing_time\\railing_images\\souqence_jingliu.png',confidence=.9)
+    click_image('railing_time\\railing_images\\add_button.png',confidence=.9)
+    
+    """
+    click_image('railing_time\\railing_images\\start_challenge.png',confidence=.9)
+
+    ################ in combat ###############
+    click_image('railing_time\\railing_images\\auto_button.png',confidence=.5, grayscale=True)
+
+
+    while True:
+        
+        if wait_for_screen('railing_time\\railing_images\\one_more_time.png'):
+            print("one more time found...")
+            restart()
+            if wait_for_screen('railing_time\\railing_images\\replenish.png', timeout=3, confidence=0.6):
+                print("add resin found...")
+                break
+            #print("going again")
+            
+            print(f"Cycle {count} completed.\n")
+            count += 1
+
+        else:
+            print("Screen not found, program stopped")
+
+    #done spending resin
+
+    ############## claim rewards ##############
+    click_image('railing_time\\railing_images\cancel.png')
+    print("clicked cancel...")
+    time.sleep(1)
+    click_image('railing_time\\railing_images\exit.png')
+    print("clicked exit...")
+    wait_for_screen('railing_time\\railing_images\\bud.png',confidence=.5)
+    pyautogui.keyDown('esc')
+    pyautogui.keyUp('esc')
+    time.sleep(1)
+    pyautogui.keyDown('altleft')
+    click_image('railing_time\\railing_images\icon.png')
+    pyautogui.keyUp('altleft')
+    #click_image('railing_time\\railing_images\daily_icon.png')
+    time.sleep(0.2)
+    click_image('railing_time\\railing_images\daily_claim.png')
+    time.sleep(0.2)
+    click_image('railing_time\\railing_images\daily_claim.png')
+    time.sleep(0.2)
+    click_image('railing_time\\railing_images\daily_claim.png')
+    time.sleep(0.2)
+    click_image('railing_time\\railing_images\daily_claim.png')
+    time.sleep(0.2)
+    click_image('railing_time\\railing_images\daily_claim.png')
+
+
+    print("all done :)")
+    # all done :)
+    return
